6d81e560e138198e09b60e949874d98fe11b5757
Flavio <senabrasousa@gmail.com>
2026-02-28 12:03:55 +0100

    chapter 2

---
+++ scala_sandbox/src/main/scala/com/example/basics/Constructs.scala
@@ -0,0 +1,15 @@
+package com.example.basics
+
+class Constructs {
+
+  // Var declaration
+  val age:Int = 30
+
+  val name:String = "Alice"
+
+  var score:Double = 95.5
+
+  var isStudent:Boolean = true
+
+  // Type Inference
+}
--- scala_sandbox/src/main/scala/com/example/basics/ExceptionHandling.scala
+++ scala_sandbox/src/main/scala/com/example/basics/ExceptionHandling.scala
@@ -2,6 +2,20 @@ package com.example.basics
 
 class ExceptionHandling {
 
-  
+  // Traditional EH
+  try {
+    val result = 10 / 0
+  } catch {
+    case e:ArithmeticException => println("Division by Zero")
+  }
+
+  // Safer EH
+  import scala.util.Try
+  val result = Try(10 / 0)
+
+  result match {
+    case scala.util.Success(value) => println(s"Result: $value")
+    case scala.util.Failure(exception) => println(s"Error: ${exception.getMessage}")
+  }
 
 }
